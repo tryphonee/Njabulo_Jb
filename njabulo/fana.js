137a4caa4a3ce98083f5ed159796fe1ba0c10b31
NjabuloJ <fanajb65@gmail.com>
2025-04-14 18:57:41 +0200

    Add files via upload

---
+++ njabulo/fana.js
@@ -0,0 +1,17 @@
+var tabCmds = [];
+let cm = [];
+function fana(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🚔";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { fana, Module: fana, cm };
+
