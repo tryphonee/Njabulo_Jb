3a5823c007836be2650566210e83d4bc06b099f3
NjabuloJ <fanajb65@gmail.com>
2025-04-21 21:27:41 +0200

    Update fana.js

--- njabulo/fana.js
+++ njabulo/fana.js
@@ -6,7 +6,7 @@ function fana(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🚘";
+        infoComs.reaction = "🧃";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
