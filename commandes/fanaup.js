2717082fb624a06b49e458894fa4abb08922da2b
NjabuloJ <fanajb65@gmail.com>
2025-04-25 15:16:18 +0200

    Create fanaup.js

---
+++ commandes/fanaup.js
@@ -0,0 +1,59 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
+
+const THUMBNAIL_URL = "https://files.catbox.moe/omgszj.jpg"; // New image URL
+
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+zokou({ nomCom: 'uptime',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '☘️', 
+    fromMe: 'true', 
+
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+      
+    try {
+        await zk.sendMessage(dest, { 
+          text: "njabulo",
+          contextInfo: {
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363345407274799@newsletter',
+          newsletterName: "NJABULO JB",
+          serverMessageId: 143,
+         },
+          forwardingScore: 999, // Score to indicate it has been forwarded
+          externalAdReply: {
+          title: "🦋ɴᴊᴀʙᴜʟᴏ ᴊʙ🦋",
+          body: ` UPTIME: ${runtime(process.uptime())}`,
+          thumbnailUrl: THUMBNAIL_URL,
+          mediaType: 1,
+          renderSmallThumbnail: true // Small thumbnail rendering
+                }
+            }
+        }, { quoted: ms });
+
+    } catch (e) {
+        console.log("❌ uptime Command Error: " + e);
+        repondre("❌ Error: " + e);
+    }
+});
+
