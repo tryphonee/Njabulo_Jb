2e8a2043897f6327d7c288288f11b60a96157f97
NjabuloJ <fanajb65@gmail.com>
2025-04-28 11:31:20 +0200

    Update pin.js

--- commandes/pin.js
+++ commandes/pin.js
@@ -6,9 +6,6 @@ const os = require("os");
 const moment = require("moment-timezone");
 const set = require(__dirname + "/../set");
 
-const AUDIO_URL = "https://files.catbox.moe/o6d4l7.mp3"; // New audio URL
-const THUMBNAIL_URL = "https://files.catbox.moe/omgszj.jpg"; // New image URL
-
 moment.tz.setDefault(`${set.TZ}`);
 
 const getTimeAndDate = () => {
