5388ae548672f86c664363a27dc2e64148229021
NjabuloJ <fanajb65@gmail.com>
2025-04-29 19:40:43 +0200

    Create fanamg.js

---
+++ commandes/fanamg.js
@@ -0,0 +1,48 @@
+const { fana } = require('../njabulo/fana');
+const gis = require('g-i-s');
+const axios = require('axios');
+const conf = require(__dirname + '/../set');
+
+fana({
+  nomCom: "img",
+  aliases: ["image", "images"],
+  categorie: "Images",
+  reaction: "☘️"
+}, async (dest, zk, commandeOptions) => {
+  const { repondre, ms, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre('Which image?');
+    return;
+  }
+
+  const searchTerm = arg.join(" ");
+  gis(searchTerm, (error, results) => sendImage(error, results));
+
+  function sendImage(error, results) {
+    if (error) {
+      repondre("Oops, an error occurred.");
+      return;
+    }
+
+    if (!results || results.length === 0) {
+      repondre("No images found.");
+      return;
+    }
+
+    for (let i = 0; i < Math.min(results.length, 5); i++) {
+      zk.sendMessage(dest, {
+        image: { url: results[i].url },
+        caption: `> Downloaded and enjoy imgs`,
+        contextInfo: {
+         isForwarded: true,
+         forwardedNewsletterMessageInfo: {
+         newsletterJid: '120363345407274799@newsletter',
+         newsletterName: "╭••➤®Njabulo Jb",
+         serverMessageId: 143,
+          }
+        }
+      }, { quoted: ms });
+    }
+  }
+});
