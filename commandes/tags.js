7b63287eb5d174386111ff26cd0b6972a322baee
NjabuloJ <fanajb65@gmail.com>
2025-05-04 23:09:01 +0200

    Create tags.js

---
+++ commandes/tags.js
@@ -0,0 +1,72 @@
+
+const { fana } = require("../njabulo/fana")
+//const { getGroupe } = require("../bdd/groupe")
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
+const {ajouterOuMettreAJourJid,mettreAJourAction,verifierEtatJid} = require("../bdd/antilien")
+const {atbajouterOuMettreAJourJid,atbverifierEtatJid} = require("../bdd/antibot")
+const { search, download } = require("aptoide-scraper");
+const fs = require("fs-extra");
+const conf = require("../set");
+const { default: axios } = require('axios');
+//const { uploadImageToImgur } = require('../njabulo/imgur');
+
+
+
+
+
+fana({ nomCom: "tags", categorie: 'Group', reaction: "📣" }, async (dest, zk, commandeOptions) => {
+
+  const { ms, repondre, arg, verifGroupe, nomGroupe, infosGroupe, nomAuteurMessage, verifAdmin, superUser } = commandeOptions
+
+
+ 
+
+  if (!verifGroupe) { repondre("✋🏿 ✋🏿this command is reserved for groups ❌"); return; }
+  if (!arg || arg === ' ') {
+  mess = 'Aucun Message'
+  } else {
+    mess = arg.join(' ')
+  } ;
+  let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
+  var tag = ""; 
+  tag +=`
+  
+ *🟢online🟢*
+`;
+
+
+  let emoji = ['•']
+  let random = Math.floor(Math.random() * (emoji.length - 1))
+
+
+  for (const membre of membresGroupe) {
+    tag += `${emoji[random]}      @${membre.id.split("@")[0]}\n`
+  }
+
+ 
+ if (verifAdmin || superUser) {
+
+  zk.sendMessage(dest, {
+        text: tag, 
+        contextInfo: {
+           isForwarded: true,
+           forwardedNewsletterMessageInfo: {
+           newsletterJid: '120363345407274799@newsletter',
+             newsletterName: "╭••➤®Njabulo Jb",
+              serverMessageId: 143,
+                },
+                forwardingScore: 999, // Score to indicate it has been forwarded
+                 externalAdReply: {
+                  title: nomGroupe,
+                  body: "📃online is alwaysonline",
+                  thumbnailUrl: conf.URL,
+                  mediaType: 1,
+                  renderSmallThumbnail: true // Small thumbnail rendering
+                }
+            }
+        }, { quoted: ms });
+
+   } else { repondre('command reserved for admins')}
+
+});
+  
