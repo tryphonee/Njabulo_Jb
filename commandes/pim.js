79fec0f8ecd01b57365d84e843b120476c64d435
NjabuloJ <fanajb65@gmail.com>
2025-05-10 23:19:30 +0200

    Create pim.js

---
+++ commandes/pim.js
@@ -0,0 +1,53 @@
+const util = require('util');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { fana } = require(__dirname + "/../njabulo/fana");
+const os = require("os");
+const moment = require("moment-timezone");
+const set = require(__dirname + "/../set");
+
+const AUDIO_URL = "https://files.catbox.moe/3o8hia.mp3"; // New audio URL
+const THUMBNAIL_URL = "https://files.catbox.moe/xfmnjg.jpg"; // New image URL
+
+moment.tz.setDefault(`${set.TZ}`);
+
+const getTimeAndDate = () => {
+    return {
+        time: moment().format('HH:mm:ss'),
+        date: moment().format('DD/MM/YYYY')
+    };
+};
+
+// Ping Command
+fana({ nomCom: "png", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms } = commandeOptions;
+    const { time, date } = getTimeAndDate();
+    const ping = Math.floor(Math.random() * 100) + 1; // Generate a random ping between 1ms - 100ms
+
+    try {
+        await zk.sendMessage(dest, { 
+           caption: ping,
+            contextInfo: {
+              isForwarded: true,
+              forwardedNewsletterMessageInfo: {
+              newsletterJid: '120363345407274799@newsletter',
+                  newsletterName: "vw golf",
+                  serverMessageId: 143,
+                   },
+                   forwardingScore: 999, // Score to indicate it has been forwarded
+                   externalAdReply: {
+                    title: "VW GOLF",
+                    body: `🧃vw golf ping: ${ping}ms\n📅 *Date:* ${date}\n⏰ *Time:* ${time}`,
+                    thumbnailUrl: THUMBNAIL_URL,
+                    mediaType: 1,
+                    renderSmallThumbnail: true // Small thumbnail rendering
+                }
+            }
+        }, { quoted: ms });
+
+    } catch (e) {
+        console.log("❌ Ping Command Error: " + e);
+        repondre("❌ Error: " + e);
+    }
+});
+
