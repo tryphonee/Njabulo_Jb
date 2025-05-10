59cb29a8be423857c1bc3bdc7d538a31cca166e1
NjabuloJ <fanajb65@gmail.com>
2025-05-11 00:35:24 +0200

    Update pim.js

--- commandes/pim.js
+++ commandes/pim.js
@@ -1,53 +1,61 @@
-const util = require('util');
-const fs = require('fs-extra');
-const axios = require('axios');
-const { fana } = require(__dirname + "/../njabulo/fana");
-const os = require("os");
+const { fana } = require("../njabulo/fana");
 const moment = require("moment-timezone");
-const set = require(__dirname + "/../set");
+const { getBuffer } = require("../njabulo/dl/Function");
+const { default: axios } = require('axios');
 
-const AUDIO_URL = "https://files.catbox.moe/3o8hia.mp3"; // New audio URL
-const THUMBNAIL_URL = "https://files.catbox.moe/xfmnjg.jpg"; // New image URL
+const AUDIO_URL = "https://files.catbox.moe/7ydtb3.mp3"; // New audio URL
+const THUMBNAIL_URL = "https://files.catbox.moe/bw9rme.jpg"; // New image URL
 
-moment.tz.setDefault(`${set.TZ}`);
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
 
-const getTimeAndDate = () => {
-    return {
-        time: moment().format('HH:mm:ss'),
-        date: moment().format('DD/MM/YYYY')
-    };
-};
 
-// Ping Command
-fana({ nomCom: "png", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms } = commandeOptions;
-    const { time, date } = getTimeAndDate();
-    const ping = Math.floor(Math.random() * 100) + 1; // Generate a random ping between 1ms - 100ms
+fana({ nomCom: 'uptime',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '📃', 
+    fromMe: 'true', 
 
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+      
     try {
         await zk.sendMessage(dest, { 
-           caption: `↪️𝘁𝗶𝗺𝗲 𝗽𝗶𝗻𝗴 ${ping}ms`,
-            contextInfo: {
-              isForwarded: true,
-              forwardedNewsletterMessageInfo: {
-              newsletterJid: '120363345407274799@newsletter',
-                  newsletterName: "vw golf",
-                  serverMessageId: 143,
-                   },
-                   forwardingScore: 999, // Score to indicate it has been forwarded
-                   externalAdReply: {
-                    title: "VW GOLF",
-                    body: "0",
-                    thumbnailUrl: THUMBNAIL_URL,
-                    mediaType: 1,
-                    renderSmallThumbnail: true // Small thumbnail rendering
+        text: `${runtime}`,
+          contextInfo: {
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363345407274799@newsletter',
+          newsletterName: "╭••➤®Njabulo Jb",
+          serverMessageId: 143,
+         },
+          forwardingScore: 999, // Score to indicate it has been forwarded
+          externalAdReply: {
+          title: "bot is alwaysonline",
+          body: "bot",
+          thumbnailUrl: THUMBNAIL_URL,
+          mediaType: 1,
+          renderSmallThumbnail: true // Small thumbnail rendering
                 }
             }
         }, { quoted: ms });
 
     } catch (e) {
-        console.log("❌ Ping Command Error: " + e);
+        console.log("❌ uptime Command Error: " + e);
         repondre("❌ Error: " + e);
     }
 });
 
+                             
